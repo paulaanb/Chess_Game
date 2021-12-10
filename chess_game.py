830bf73471929f8c705a6c244f4a736666628f25
paulaanb <paulanb2000@gmail.com>
2021-12-10 11:09:46 +0100

    Creamos un bucle y lo finalizamos

--- chess_game.py
+++ chess_game.py
@@ -8,9 +8,30 @@ def chess_game(fichier_name):
     for i in initial_board.split('\n'):
         # Dividimos al tablero la lista que resulta de dividir la subcadena i por el caracter de tabulación.
         board.append(i.split('\t'))
-    # Ponemos el fichero en modo lectura y creamos un bucle para recorrer las filas del tablero
+    # Ponemos el archivo en modo lectura y creamos un bucle para recorrer las filas del tablero
     f = open(fichier_name, 'w')
     for i in board:
         f.write('\t'.join(i) + '\n')
     f.close()
     
+    movement = 0
+    # Creamos un bucle para realizar movimientos en la partida hasta que el usario decida terminar.
+    while True:
+        continuar = input('¿Desea realizar algún otro movimiento? (Si/ No):')
+        if continuar != 'Si':
+            break
+        else:
+            original_line = int(input('Por favor introduzca la fila de la pieza que desea mover: '))
+            original_column = int(input('Por favor introduzca la columna de la pieza que desea mover: '))
+            destination_line = int(input('Por favor introduzca la fila a donde desea mover la pieza: '))
+            destination_column = int(input('or favor introduzca la columna a donde desea mover la pieza: '))
+            board[destination_line-1][destination_column-1] = board[original_line-1][original_column-1]
+            board[original_line-1][original_column-1] = ''
+            movement += 1
+            # Abrimos el archivo en modo añadir y añadimos una cadena con el número de movimientos
+            f = open(fichier_name, 'a')
+            f.write('Movimiento' + str(movement) + '\n')
+            # Creamos un bucle para recorrer las filas del tablero y escribimos cada fila en una linea
+            for i in board:
+                f.write('\t'.join(i) + '\n')
+            f.close()
